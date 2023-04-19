a4f653601d17f8a8240edc48f84f484100336dd3
jny0 <94813918+jny0@users.noreply.github.com>
2023-04-19 15:44:20 +0900

    [level 3] Title: 네트워크, Time: 0.31 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,6 +1,32 @@
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
 class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        boolean[] check = new boolean[n];
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (check[i]) continue;
+
+            check[i] = true;
+            queue.offer(computers[i]);
+
+            while (!queue.isEmpty()) {
+                int[] computer = queue.poll();
+
+                for (int j = 0; j < n; j++) {
+                    if (!check[j] && computer[j] == 1) {
+                        check[j] = true;
+                        queue.offer(computers[j]);
+                    }
+                }
+            }
+            answer++;
+        }
         return answer;
     }
 }
