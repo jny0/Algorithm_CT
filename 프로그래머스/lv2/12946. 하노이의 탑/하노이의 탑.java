37ea9c024912fec7a3e67004b06fe3b9d147048c
jny0 <94813918+jny0@users.noreply.github.com>
2023-04-19 10:12:05 +0900

    [level 2] Title: 하노이의 탑, Time: 3.36 ms, Memory: 94.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12946. 하노이의 탑/하노이의 탑.java
@@ -0,0 +1,30 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    static int count = 0;
+    static int[][] answer;
+    public int[][] solution(int n) {
+        answer = new int[(int)(Math.pow(2,n)-1)][2];
+        
+        Hanoi(n, 1, 2, 3);
+        
+        return answer;
+    }
+
+    public static void Hanoi(int n, int start, int mid, int end) {
+        if (n == 1) {
+            answer[count][0] = start;
+            answer[count][1] = end;
+            count++;
+            return;
+        }
+
+        Hanoi(n - 1, start, end, mid);
+        answer[count][0] = start;
+        answer[count][1] = end;
+        count++;
+        Hanoi(n - 1, mid, start, end);
+    }
+}
