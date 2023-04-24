1524aebb23c73cb599690c53445e9801b6e6d337
jny0 <94813918+jny0@users.noreply.github.com>
2023-04-24 10:29:08 +0900

    [level 0] Title: k의 개수, Time: 2.16 ms, Memory: 67.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120887. k의 개수/k의 개수.java
+++ 프로그래머스/lv0/120887. k의 개수/k의 개수.java
@@ -3,16 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int i, int j, int k) {
         int answer = 0;
-        String[] numList = new String[j-i +1];
-        
-        for(int n = i; n<=j; n++){
-            numList[n-i] = String.valueOf(n);
-        }
 
-        for(int a = 0; a<numList.length; a++){
-            for(int b=0; b<numList[a].length(); b++){
-                int num = (int)(numList[a].charAt(b) - '0');
-                if(num == k) answer++;
+        for (int num = i; num <= j; num++){
+            int tmp = num;
+            while (tmp != 0){
+                if (tmp % 10 == k)
+                    answer++;
+                tmp /= 10;
             }
         }
         return answer;
