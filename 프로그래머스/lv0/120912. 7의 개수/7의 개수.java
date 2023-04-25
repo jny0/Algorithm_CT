77b3d2a8a1e63f5524e34ad1743a98ad13d640da
jny0 <94813918+jny0@users.noreply.github.com>
2023-04-25 10:26:44 +0900

    [level 0] Title: 7의 개수, Time: 0.21 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i : array){
+            sb.append(i);
+        }
+        
+        for(int i=0; i<sb.length(); i++){
+            if(sb.charAt(i) == '7') answer++;
+        }
+        
+        System.out.println(sb);
+        
+        return answer;
+    }
+}
