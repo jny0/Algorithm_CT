14c8495fb6d0c58ae303875051f024f0aaf6fe04
jny0 <94813918+jny0@users.noreply.github.com>
2023-04-25 09:24:18 +0900

    [level 0] Title: 공 던지기, Time: 0.05 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -1,44 +1,16 @@
+
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
         int count = 0;
-        int length = numbers.length;
-       
-        if(length%2 == 0){
-            while(count != k){
-                for(int i=1; i<=length; i+=2){
-                    count++;
-                    if(count == k) {
-                        answer = i;
-                        break;
-                    }
-                }
+        int answer = 0;
+        for(int i=0; i<k-1; i++) {
+            count = count + 2;
+            if(count > numbers.length-1) {
+               count =  count - numbers.length;
             }
-        } else {
-           while(count != k){
-                for(int i=1; i<=length; i+=2){
-                    count++;
-                    if(count == k) {
-                        answer = i;
-                        break;
-                    }
-                }
-               
-               if(count == k) {
-                    break;
-               }
-               
-               
-                for(int i=2; i<length; i+=2){
-                    count++;
-                    if(count == k) {
-                        answer = i;
-                        break;
-                    }
-                }
+            answer = numbers[count];
+        }
 
-            }
-        }   
         return answer;
     }
 }
