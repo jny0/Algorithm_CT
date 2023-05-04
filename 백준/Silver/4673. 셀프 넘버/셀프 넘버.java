13465e0bdcf6c2d4a6dc5c1c654403def297dfac
jny0 <94813918+jny0@users.noreply.github.com>
2023-05-04 17:24:42 +0900

    [Silver V] Title: 셀프 넘버, Time: 148 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        int N = 10000;
+        int[] arr = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = i;
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+         for (int i = 1; i <N; i++) {
+            if(arr[i] != 0){
+                int num = arr[i];
+                sb.append(num).append("\n");
+                int sum = arr[i];
+                while(sum < N){
+                    while(true){
+                        sum += num%10;
+                        num = num / 10;
+                        if(sum > N) break;
+                        if(num == 0){
+                            arr[sum] = 0;
+                            break;
+                        }
+                    }
+                    num = sum;
+                }
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
