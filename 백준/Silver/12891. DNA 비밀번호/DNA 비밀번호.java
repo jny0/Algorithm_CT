2668bddc0648f5e64c18efe7a439543526acaed8
jny0 <94813918+jny0@users.noreply.github.com>
2023-05-14 03:08:19 +0900

    [Silver II] Title: DNA 비밀번호, Time: 308 ms, Memory: 20048 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -5,72 +5,69 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int[] checkCount = new int[4];
-    static int[] count = new int[4];
+    static int[] requiredCount = new int[4];
+    static int[] currentCount = new int[4];
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int S = Integer.parseInt(st.nextToken());
-        int P = Integer.parseInt(st.nextToken());
+        int dnaLength = Integer.parseInt(st.nextToken());
+        int substringLength = Integer.parseInt(st.nextToken());
 
-        String str = br.readLine();
-
+        String dna = br.readLine();
 
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < checkCount.length; i++) {
-            checkCount[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < requiredCount.length; i++) {
+            requiredCount[i] = Integer.parseInt(st.nextToken());
         }
 
         int answer = 0;
 
-        for (int i = 0; i < P; i++) {
-            count[dnaCheck(str.charAt(i))]++;
+        for (int i = 0; i < substringLength; i++) {
+            currentCount[dnaToIndex(dna.charAt(i))]++;
         }
-        if(isValid()) answer++;
+        if(isValidSubstring()) answer++;
 
 
-        for (int right = P; right < S; right++) {
-            int left = right-P;
-            count[dnaCheck(str.charAt(right))]++;
-            count[dnaCheck(str.charAt(left))]--;
+        for (int right = substringLength; right < dnaLength; right++) {
+            int left = right-substringLength;
+            currentCount[dnaToIndex(dna.charAt(right))]++;
+            currentCount[dnaToIndex(dna.charAt(left))]--;
 
-            if(isValid()) answer++;
+            if(isValidSubstring()) answer++;
         }
 
         System.out.println(answer);
-
     }
 
-    public static int dnaCheck(char a){
-        int result = 0;
+    public static int dnaToIndex(char a){
+        int index = 0;
         switch(a){
             case 'A' :
-                result =  0;
+                index =  0;
                 break;
             case 'C' :
-                result =  1;
+                index =  1;
                 break;
             case 'G' :
-                result =  2;
+                index =  2;
                 break;
             case 'T' :
-                result =  3;
+                index =  3;
                 break;
         }
-        return result;
+        return index;
     }
 
-    public static boolean isValid() {
+    public static boolean isValidSubstring() {
 
         for (int i = 0; i < 4; i++) {
-            if(checkCount[i] > count[i]){
+            if(requiredCount[i] > currentCount[i]){
                 return false;
             }
         }
 
         return true;
     }
-
 }
