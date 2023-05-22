e6b781de5eea276ea3821f10b452d271481a6cd2
jny0 <94813918+jny0@users.noreply.github.com>
2023-05-22 11:11:17 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] number) {
+        int answer = 0;
+        
+        int n = number.length;
+        
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                for(int k=j+1; k<n; k++){
+                    int sum = number[i] + number[j] + number[k];
+                    if(sum == 0) answer ++;
+                }                    
+            }
+        }
+        
+        
+        return answer;
+    }
+}
