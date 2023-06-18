0420092f8580bfa2ae23e3648603e52eaa613eb0
jny0 <94813918+jny0@users.noreply.github.com>
2023-06-18 10:45:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.05 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
 
 ### 성능 요약
 
-메모리: 53.6 MB, 시간: 6.74 ms
+메모리: 56.2 MB, 시간: 6.05 ms
 
 ### 구분
 
--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,27 +1,27 @@
 import java.util.*;
+
 class Solution {
-    
     static int visited[][];
     static Queue<Node> queue;
     static int dx[] = {0, 1, 0, -1};
     static int dy[] = {1, 0, -1, 0};
     
     public int solution(int[][] maps) {
-        int answer = 0;
+        int answer = -1;
         int n = maps.length;
         int m = maps[0].length;
         
         visited = new int[n][m];
         queue = new LinkedList<>();
-
-        bfs(maps, n, m);
-        answer = visited[n-1][m-1];
         
-        if(answer == 0) answer = -1;
+        bfs(maps, n, m);
+        if(visited[n-1][m-1] != 0){
+            answer = visited[n-1][m-1];
+        }
+        
         
         return answer;
     }
-    
     
     public static void bfs(int[][] maps, int n, int m){
         visited[0][0] = 1;
@@ -30,18 +30,25 @@ class Solution {
         while(!queue.isEmpty()){
             Node node = queue.poll();
             
-            for(int k=0; k<4; k++){
-                int nx = node.x + dx[k];
-                int ny = node.y + dy[k];
-                
-                if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-
-                if(visited[nx][ny] == 0 && maps[nx][ny] == 1){
+             for(int k=0; k<4; k++) {
+                 int nx = node.x + dx[k];
+                 int ny = node.y + dy[k];
+                 
+                 if(nx < 0 || ny < 0 || nx >=n || ny>= m || visited[nx][ny] != 0){
+                     continue;
+                 }
+                 
+                if(maps[node.x][node.y] == 1){
                     visited[nx][ny] = visited[node.x][node.y] +1;
-                    queue.add(new Node(nx, ny));
-                }     
+                    queue.offer(new Node(nx, ny));
+                }  
+                 
+             }
+            
             }
-        }
+            
+            
+        
     }
     
 }
