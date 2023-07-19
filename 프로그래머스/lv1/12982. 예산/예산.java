50fb54669f8f89994cf56a6668dd24b8f7e63e61
jny0 <94813918+jny0@users.noreply.github.com>
2023-07-19 21:29:07 +0900

    [level 1] Title: 예산, Time: 0.48 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        for(int amount : d){
+            if(budget < amount){
+                break;
+            }
+            budget -= amount;
+            answer++;
+        }
+        
+        
+        return answer;
+    }
+}
