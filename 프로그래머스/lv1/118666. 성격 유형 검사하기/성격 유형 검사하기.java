71ec1be9afc220a53260045e8153972d5d475771
jny0 <94813918+jny0@users.noreply.github.com>
2023-08-01 20:14:17 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 12.13 ms, Memory: 81.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,43 @@
+import java.util.*;
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+       List<String> answerList = new ArrayList<>(Arrays.asList("RT", "CF", "JM", "AN"));
+        
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<survey.length; i++){
+            String type = survey[i];
+            if(answerList.contains(type)){
+                 map.put(type, map.getOrDefault(type, 0) + (4 - choices[i]));
+            } else{
+                type = switchSurveyAnswer(type);
+                map.put(type, map.getOrDefault(type, 0) + (choices[i] - 4));
+            }
+        }
+        
+        List<String> list = new ArrayList<>();
+        
+        for(String key : map.keySet()){
+            System.out.println(key + " : " + map.get(key));
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i=0; i<answerList.size(); i++){
+            int score = map.getOrDefault(answerList.get(i), 1);
+            if(score >= 0){
+               answer.append(answerList.get(i).charAt(0));
+            }else{
+                answer.append(answerList.get(i).charAt(1));
+            }
+        }
+        
+        return answer.toString();
+    }
+    
+    public String switchSurveyAnswer(String answer){
+        StringBuilder newAnswer = new StringBuilder();
+        newAnswer.append(answer.charAt(1));
+        newAnswer.append(answer.charAt(0));
+        return newAnswer.toString();
+    }
+}
