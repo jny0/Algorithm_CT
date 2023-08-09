5dd98635ea74a11d482c114d069e428a43cf5e99
jny0 <94813918+jny0@users.noreply.github.com>
2023-08-09 19:54:35 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.26 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = new_id.toLowerCase().replaceAll("[^a-z0-9\\-_\\.]","")
+            .replaceAll("\\.{2,}", "\\.").replaceAll("^\\.|\\.$", "");
+        
+        if(answer.isEmpty()) answer = "a";
+        if(answer.length() >= 16) answer = answer.substring(0,15).replaceAll("\\.$", "");
+        while (answer.length() <= 2) {
+            answer += answer.charAt(answer.length() - 1);
+        }
+        
+        return answer;
+    }
+}
