c459feb499c88e2f64fa216586b3c95ae3f0b5ff
jny0 <94813918+jny0@users.noreply.github.com>
2023-08-21 14:26:44 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.29 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -1,33 +1,24 @@
 import java.util.*;
+
 class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
 
         for(int i=0; i<n; i++){
-            String[] binary1 = changeToBinaryString(arr1[i], n);
-            String[] binary2 = changeToBinaryString(arr2[i], n);
-            StringBuilder sb = new StringBuilder();
-
-            for(int j=0; j<n; j++){
-                if(binary1[j].equals("0") && binary2[j].equals("0")){
-                    sb.append(" ");
-                }else{
-                    sb.append("#");
-                }
-            }
-            answer[i] = sb.toString();
+            String binary = changeToBinaryString(arr1[i]|arr2[i], n);
+            answer[i] = binary.replaceAll("1","#").replaceAll("0"," ");
         }
 
         return answer;
     }
 
-    public static String[] changeToBinaryString(int a, int n){
+    public static String changeToBinaryString(int a, int n){
         String binary = Integer.toBinaryString(a);
         if (binary.length() < n) {
             int diff = n - binary.length();
             String padding = "0".repeat(diff);
             binary = padding + binary;
         }
-        return binary.split("");
+        return binary;
     }
 }
