c311931ef01a847e44303e7d46193680db61ee6f
박진영 <94813918+jny0@users.noreply.github.com>
2023-08-25 13:47:40 +0900

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,22 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n) {
         int answer = 0;
-
-        List<Integer> list = new ArrayList<>();
-
+        
         for (int i = 1; i <= Math.sqrt(n); i++){
-            if(n%i == 0) list.add(i);
+            if(i*i == n){
+                 answer += i;
+            }else if(n%i == 0) {
+                answer += (i + n/i);
+            }
         }
-
-        int size = list.size();
-
-        for (int i = 0; i < size; i++){
-            if(n%list.get(i) == 0) list.add(n/list.get(i));
-        }
-
-        answer = list.stream().distinct().mapToInt(Integer::intValue).sum();
 
         return answer;
     }
