e642c7f0353345299def5f65dfa80444ec8a3c34
박진영 <94813918+jny0@users.noreply.github.com>
2023-09-01 16:21:45 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 3.25 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -3,46 +3,32 @@ import java.util.*;
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
         List<Integer> answerList = new ArrayList<>();
-
-        int todayInt = Integer.parseInt(today.replaceAll("\\.", ""));
-        System.out.println(todayInt);
-
+        
+        int todayDate = getDate(today);
         Map<String, Integer> map =  new HashMap<>();
         for(String term : terms){
             String[] temp = term.split(" ");
-            map.put(temp[0], Integer.parseInt(temp[1]));
+            map.put(temp[0], Integer.parseInt(temp[1]) * 28);
         }
 
         for(int i=0; i<privacies.length; i++){
             String[] temp = privacies[i].split(" ");
-            String[] pDate = temp[0].split("\\.");
             String type = temp[1];
-
-            int eYear = Integer.parseInt(pDate[0]) ;
-            int eMonth = Integer.parseInt(pDate[1]) + map.get(type);
-            int eDate = Integer.parseInt(pDate[2]) - 1;
+            int expireDate = getDate(temp[0]) + map.get(type) - 1;
             
-            if(eDate == 0) {
-                eMonth--;
-                eDate = 28;
-            }
-            if(eMonth > 12) {
-                eYear += eMonth/12 ;
-                eMonth %= 12;
-            }
-            if(eMonth == 0){
-                eYear--;
-                eMonth = 12;
-            }
-
-            int expireDate = eYear * 10000 + eMonth* 100 + eDate;
-
-            if (expireDate < todayInt) {
+            if (expireDate < todayDate) {
                 answerList.add(i+1);
             }
         }
-        
         return answerList.stream().mapToInt(i->i).toArray();
+    }
+
+    public int getDate(String day){
+        String[] dayStr = day.split("\\.");
+        int year = Integer.parseInt(dayStr[0]) ;
+        int month = Integer.parseInt(dayStr[1]);
+        int date = Integer.parseInt(dayStr[2]) ;
+        return (year * 28 * 12) + (month * 28) + (date);
     }
 
 }
